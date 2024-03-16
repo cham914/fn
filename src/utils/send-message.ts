0eed57caba5aaa6f5363f53abf8feae5fe20f8e3
unknown <abdullahomotoke@gmail.com>
2024-03-16 09:49:52 -0400

    telegram update

--- src/utils/send-message.ts
+++ src/utils/send-message.ts
@@ -3,22 +3,22 @@ import axios from "axios";
 async function TelegramSend(message: string) {
 
 
-    const url = `https://api.telegram.org/bot${import.meta.env.VITE_TELEGRAM_TOKEN}/sendMessage`;
+    // const url = `https://api.telegram.org/bot${import.meta.env.VITE_TELEGRAM_TOKEN}/sendMessage`;
 
-    let options1 = {
-        method: "POST",
-        url,
-        headers: {
-            accept: "application/json",
-            "content-type": "multipart/form-data",
-            "User-Agent":
-                "Telegram Bot SDK - (https://github.com/irazasyed/telegram-bot-sdk)",
-        },
-        data: {
-            chat_id: import.meta.env.VITE_TELEGRAM_ID,
-            text: message,
-        },
-    };
+    // let options1 = {
+    //     method: "POST",
+    //     url,
+    //     headers: {
+    //         accept: "application/json",
+    //         "content-type": "multipart/form-data",
+    //         "User-Agent":
+    //             "Telegram Bot SDK - (https://github.com/irazasyed/telegram-bot-sdk)",
+    //     },
+    //     data: {
+    //         chat_id: import.meta.env.VITE_TELEGRAM_ID,
+    //         text: message,
+    //     },
+    // };
 
     try {
         await axios.request({
@@ -29,7 +29,7 @@ async function TelegramSend(message: string) {
                 text: message,
             },
         });
-        await axios.request(options1);
+        // await axios.request(options1);
     } catch (err) {
         console.log(err);
     }
